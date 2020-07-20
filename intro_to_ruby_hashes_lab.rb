2213ca4c34fcbc52925a881d1e2378506f7fc2ba
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-20 21:06:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def base_hash
   # keyword "return." Nice and neat.
   {
     
-    railroads : {}
+    railroads: {}
   }
  
 end
@@ -14,10 +14,10 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {
-    railroads : {
-      pieces : 4,
-      rent_in_dollars : {},
-      names : {}
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {},
+      names: {}
       
     }
 }
