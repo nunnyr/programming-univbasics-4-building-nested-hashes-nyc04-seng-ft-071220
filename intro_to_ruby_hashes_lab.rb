4dcde644c1de504f263b7362f23c89a6bcc2db33
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-20 21:15:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,10 @@ def monopoly_with_third_tier
   {
     railroads: {
       pieces: 4,
-      rent_in_dollars: {},
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50, 
+      },
       names: {}
       
     }
